844654807605adddd5e60b3019a89fdf2b990014
okunishinishi <okunishinishi@gmail.com>
2018-12-25 11:17:06 +0900

    support older format

--- test/cipherjson_test.js
+++ test/cipherjson_test.js
@@ -5,42 +5,35 @@
 'use strict'
 
 const Cipherjson = require('../lib/cipherjson.js')
-const co = require('co')
 const assert = require('assert')
 
 describe('cipherjson', () => {
-  before(() => co(function * () {
-  }))
-
-  after(() => co(function * () {
-  }))
-
-  it('Write and read.', () => co(function * () {
+  it('Write and read.', async () => {
     let filename = `${__dirname}/../tmp/testing.secret.json`
     let cipherjson = new Cipherjson('hoge')
-    yield cipherjson.write(filename, {
+    await cipherjson.write(filename, {
       foo: 'bar'
     })
-    let data = yield cipherjson.read(filename)
-    assert.deepEqual(data, { foo: 'bar' })
-  }))
+    let data = await cipherjson.read(filename)
+    assert.equal(data.foo, 'bar')
+  })
 
-  it('Write and read sync.', () => co(function * () {
+  it('Write and read sync.', async () => {
     let filename = `${__dirname}/../tmp/testing.secret.json`
     let cipherjson = new Cipherjson('hoge')
-    yield cipherjson.write(filename, {
+    await cipherjson.write(filename, {
       foo: 'bar'
     })
     let data = cipherjson.readSync(filename)
-    assert.deepEqual(data, { foo: 'bar' })
-  }))
+    assert.equal(data.foo, 'bar')
+  })
 
-  it('Read not ciphered', () => co(function * () {
+  it('Read not ciphered', async () => {
     let filename = require.resolve('../doc/mocks/mock-data.json')
     let cipherjson = new Cipherjson('hoge')
-    let data = yield cipherjson.read(filename)
+    let data = await cipherjson.read(filename)
     assert.deepEqual(data, { foo: 'bar' })
-  }))
+  })
 })
 
 /* global describe, before, after, it */
